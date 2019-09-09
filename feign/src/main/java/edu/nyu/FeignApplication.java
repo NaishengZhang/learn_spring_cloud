8dd74d11dbc891f471f249bcad086dcfeafdaaad
Naisheng Zhang <jonathan.n.zhang@gmail.com>
2019-09-09 19:22:19 -0400

    Add feign

--- feign/src/main/java/edu/nyu/FeignApplication.java
+++ feign/src/main/java/edu/nyu/FeignApplication.java
@@ -1,4 +1,13 @@
 package edu.nyu;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
 public class FeignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FeignApplication.class, args);
+    }
 }
