951babe052b2a9a4e89e73ff5eeaa435fd1da04b
Naisheng Zhang <jonathan.n.zhang@gmail.com>
2019-09-09 18:14:38 -0400

    Add Ribbon

---
+++ ribbon/src/main/java/edu/nyu/entity/Student.java
@@ -0,0 +1,16 @@
+package edu.nyu.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    private long id;
+    private String name;
+    private int age;
+
+}
